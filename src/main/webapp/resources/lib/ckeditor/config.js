6ada1943cece872b74049bfba2c46e2b19b0d361
leafgreen <leafgreen.hwang@gmail.com>
2016-06-13 20:35:35 +0900

    ckeditor config.js 적용

--- src/main/webapp/resources/lib/ckeditor/config.js
+++ src/main/webapp/resources/lib/ckeditor/config.js
@@ -16,4 +16,9 @@ CKEDITOR.editorConfig = function( config ) {
 		useJSON: false,
 		useColorIcon: false
 	};
+	
+	config.autosaveTargetUrl = 'http://example.com/path_to_script_that_saves_data';
+	
+	config.font_names = '돋움; Nanum Gothic Coding; 맑은 고딕; 바탕; 궁서; Quattrocento Sans;' +  
+	    CKEDITOR.config.font_names;
 };
